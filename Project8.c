b8b9fb136c6e5511a99f10250c108d22f346e388
Matthew <mjulio@nevada.unr.edu>
2020-04-27 21:47:21 -0700

    Basic functions

--- Project8.c
+++ Project8.c
@@ -1,11 +1,51 @@
 //Matthew Julio, Esham Mahmud, NAME
 //due DATE
 //This is a connect 4 game that will display the hisoric wins to the user
-//test
+
 #include<stdio.h>
+
 int main (void)
 {
-	printf("Hello World!\n");
-    printf("Goodbye\n");
+	
 	return 0;
+}
+
+void getMenCoice (void)
+{
+	printf("");
+}
+
+void display (void)
+{
+
+}
+
+void checkForWin (void)
+{
+
+}
+
+void displayWins (void)
+{
+
+}
+
+void turnTracker (void)
+{
+
+}
+
+void recordWin (void)
+{
+
+}
+
+void getUserInput (void)
+{
+	scanf("", );
+}
+
+void updateArray (void)
+{
+
 }
